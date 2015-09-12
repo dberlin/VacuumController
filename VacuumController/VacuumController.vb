0ccc233c85470948ce3d733840c851b60086bcda
Daniel Berlin <dberlin@dberlin.org>
2015-09-12 10:24:47 -0700

    Update to new version of advancedhmi

--- VacuumController/VacuumController.vb
+++ VacuumController/VacuumController.vb
@@ -1,48 +1,4 @@
 ﻿Public Class VacuumController
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        'Select Case CInt(AddressFirstDigit)
-        '    Case 0 : m_ReadFunctionCode = 1
-        '        WriteSingleFunctionCode = 5
-        '        WriteMultiFunctionCode = 15
-        '        m_BitsPerElement = 1
-        '    Case 1 : m_ReadFunctionCode = 2
-        '        WriteMultiFunctionCode = 0 '* Invalid
-        '        m_BitsPerElement = 1
-        '    Case 3 : m_ReadFunctionCode = 4
-        '        WriteMultiFunctionCode = 0
-        '        m_Element = CInt(Address.Substring(1)) - 1
-        '        m_BitsPerElement = 16
-        '    Case 4 : m_ReadFunctionCode = 3
-        '        WriteSingleFunctionCode = 6
-        '        WriteMultiFunctionCode = 16
-        '        m_Element = CInt(Address.Substring(1)) - 1
-        '        m_BitsPerElement = 16
-    End Sub
-
-    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub MomentaryButton1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub UpButton_Click(sender As Object, e As EventArgs) Handles UpButton.Click
-
-    End Sub
-
-    Private Sub PIDSetpoint_GotFocus(sender As Object, e As EventArgs) Handles PIDSetpointVC75.GotFocus
-
-    End Sub
-
-    Private Sub PIDSetpoint_TextChanged(sender As Object, e As EventArgs) Handles PIDSetpointVC75.TextChanged
-
-    End Sub
 
     Private Sub PIDSetpoint_ValueChanged(sender As Object, e As EventArgs) Handles PIDSetpointVC75.ValueChanged
         UpButton.ValueToWrite = PIDSetpointVC75.Value + 5
@@ -56,10 +12,18 @@
     Private Sub ModBusToSV65_ConnectionEstablished(sender As Object, e As EventArgs) Handles ModBusToSV65.ConnectionEstablished
         'Unlock drive
         ModBusToSV65.Write(40049, 0)
-        'ModBusToSV65.Write(40002, 8)
     End Sub
 
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs)
 
+
+    Private Sub StartSV65Button_MouseDown(sender As Object, e As MouseEventArgs) Handles StartSV65Button.MouseDown
+        'Unlock drive after command
+        ModBusToSV65.Write(40049, 0)
+
+    End Sub
+
+    Private Sub StopSV65Button_MouseDown(sender As Object, e As MouseEventArgs) Handles StopSV65Button.MouseDown
+        'Unlock drive after command
+        ModBusToSV65.Write(40049, 0)
     End Sub
 End Class
